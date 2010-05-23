7db458bf7ff8fb3f0ba174fbcca850e73b152069
scarduzio <scarduzio@f8635a5a-d4f1-46bc-7645-58066ea36bd0>
2010-05-23 16:44:47 +0000

    Bugfix: text namespaces don't create directories

--- Tephlon/Tephlon.php
+++ Tephlon/Tephlon.php
@@ -12,18 +12,18 @@ require_once("DataStructures/TMap.php");
 class Tephlon {
 	/**
 	 * Ask Tephlon for a basic persistence resource from Core API.
-	 * Are you sure you don't want to ask for a more complete data 
+	 * Are you sure you don't want to ask for a more complete data
 	 * structure like TMap?
 	 * In that case just simply instantiate it:
-	 * 
+	 *
 	 * $map = new TMap("label");
-	 * 
+	 *
 	 * @param string|object $namespace The name of the resource you want. If a resource
 	 * with this name exists, it will be returned. With all its records.
 	 * If no resources are found with this name, Tephlon will create an empty one.
-	 * 
+	 *
 	 * @param string $driverName leave this empty, there's just File driver available.
-	 * 
+	 *
 	 * @return FileResource or false, if problems were detected in the namesapce string
 	 * or if something went wrong creating/retrieving the resource itself.
 	 */
@@ -43,7 +43,7 @@ class Tephlon {
 		}
 		return $r;
 	}
-	
+
 	private static function extractContext($label){
 		if(is_null($label)){
 			return "_global_context_";
@@ -52,7 +52,9 @@ class Tephlon {
 			$label = get_class($label).".".sha1(serialize($label));
 			return $label;
 		}
-		return PersistenceEngine::validateContext($label);
+		if(PersistenceEngine::validateContext($label)){
+			return $label;
+		}
 	}
 }
 
