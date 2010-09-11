d1d2cdd5fdbee4864a994b16648faa1b00ec8b15
scarduzio <scarduzio@f8635a5a-d4f1-46bc-7645-58066ea36bd0>
2010-09-11 18:17:40 +0000

    Enhanced footer content

--- TephlonDemo/trunk/application/views/elements/footer.php
+++ TephlonDemo/trunk/application/views/elements/footer.php
@@ -3,10 +3,10 @@
     <?php echo ul($this->WikiScraper->getWikiList())?>
 </div>
 <div class="column">
-    <h3>Examples</h3>
+    <h3>In Action</h3>
     <ul>
-        <li><a href="<?php echo $site_url ?>datastructures/tcounter_demo">Access Counter</a></li>
-        <li><a href="<?php echo $site_url ?>datastructures/tbuffer_fifo_demo">Ajax Chat</a></li>
+        <li><a href="<?php echo $site_url ?>datastructures/tcounter_demo">Page visits (TCounter)</a></li>
+        <li><a href="<?php echo $site_url ?>datastructures/tbuffer_fifo_demo">Ajax Chat (TBuffer)</a></li>
     </ul>
 </div>
 <div class="column">
@@ -21,8 +21,8 @@
 <div class="column">
     <h3>Extra</h3>
     <ul>
-        <li><a href="">Download latest</a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
+        <li><a href="http://code.google.com/p/tephlon/downloads/list">Download latest</a></li>
+        <li><a href="http://code.google.com/p/tephlon/issues/list">Report a bug</a></li>
+        <li><a href="http://code.google.com/p/tephlon/source/browse/#svn/trunk/Tephlon">Explore The Code!</a></li>
     </ul>
 </div>
